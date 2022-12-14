d2b90c1b7263b4b41b47f75883b945838147309c
Your name <emilien.lentes@gmail.com>
2022-12-14 11:06:46 +0100

    commit caroussel

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,21 +1,19 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
-
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
 
 export default function ProfileScreen() {
   return (
     <View style={styles.container}>
       <Text>ProfileScreen</Text>
     </View>
-  )
+  );
 }
 
-
-const styles = StyleSheet.create ({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'red',
-}
-})
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "red",
+  },
+});
